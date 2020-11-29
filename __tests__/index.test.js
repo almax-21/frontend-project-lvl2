53e750abf425973717785def661c2f5ec34010bc
almax-21 <almaev21.max@gmail.com>
2020-11-29 19:31:40 +0400

    Add Code Coverage

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,5 @@
 import genDiff from '../src/index.js';
 
 test("gendiff's main flow", () => { // trial version!!!
-  expect(genDiff('/home/max/Projects/tmp/file1.json', '/home/max/Projects/tmp/file2.json')).toBeTruthy();
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toBeTruthy();
 });
