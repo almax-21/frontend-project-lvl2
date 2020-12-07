2fd63cdf3337184a7b5f5ff2b3bff37bc53c3efd
almax-21 <almaev21.max@gmail.com>
2020-12-07 16:16:18 +0400

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,29 +9,29 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('stylish formatter test', () => {
-  const file1 = getFixturePath('file1.yaml');
-  const file2 = getFixturePath('file2.json');
+const cases = [
+  ['stylish', 'expected_stylish.txt'],
+  ['plain', 'expected_plain.txt'],
+  ['json', 'expected_json.txt'],
+];
+
+test.each(cases)(
+  '%p formatter test',
+  (formatName, filepath) => {
+    const file1 = getFixturePath('file1.yaml');
+    const file2 = getFixturePath('file2.json');
+    const actual = genDiff(file1, file2, formatName);
+    const expected = readFile(filepath);
+
+    expect(actual).toBe(expected);
+  },
+);
+
+test('default formatter', () => {
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.yml');
   const actual = genDiff(file1, file2);
   const expected = readFile('expected_stylish.txt');
-
-  expect(actual).toBe(expected);
-});
-
-test('plain formatter test', () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
-  const actual = genDiff(file1, file2, 'plain');
-  const expected = readFile('expected_plain.txt');
-
-  expect(actual).toBe(expected);
-});
-
-test('json formatter test', () => {
-  const file1 = getFixturePath('file1.yaml');
-  const file2 = getFixturePath('file2.yml');
-  const actual = genDiff(file1, file2, 'json');
-  const expected = readFile('expected_json.txt');
 
   expect(actual).toBe(expected);
 });
