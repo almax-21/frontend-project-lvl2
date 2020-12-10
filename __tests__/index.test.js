301c1a2b99687aa8b2c493fc0cbdbfdde822e5d6
almax-21 <almaev21.max@gmail.com>
2020-12-10 23:17:45 +0400

    Refactoring tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,18 +9,15 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const cases = [
-  ['stylish', 'expected_stylish.txt'],
-  ['plain', 'expected_plain.txt'],
-  ['json', 'expected_json.txt'],
-];
+const cases = ['stylish', 'plain', 'json'];
 
 test.each(cases)(
-  '%p formatter test',
-  (formatName, filepath) => {
+  '%p formatter test', (formatName) => {
     const file1 = getFixturePath('file1.yaml');
     const file2 = getFixturePath('file2.json');
     const actual = genDiff(file1, file2, formatName);
+
+    const filepath = `${formatName}.txt`;
     const expected = readFile(filepath);
 
     expect(actual).toBe(expected);
@@ -31,7 +28,7 @@ test('default formatter', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.yml');
   const actual = genDiff(file1, file2);
-  const expected = readFile('expected_stylish.txt');
+  const expected = readFile('stylish.txt');
 
   expect(actual).toBe(expected);
 });
