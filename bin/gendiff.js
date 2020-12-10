d73c793c4baa2cb4c39ab78dd3e8ba5611927be8
almax-21 <almaev21.max@gmail.com>
2020-12-10 14:46:58 +0400

    Update bin file && description

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
+import program from 'commander';
 import genDiff from '../src/index.js';
 
 program
-  .version('1.0.0')
+  .version('1.0.1')
   .arguments('<filepath1> <filepath2>')
-  .description('Compares two configuration files and shows a difference.')
+  .description('Compares two configuration files and shows a difference in stylish, plain and json format.')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     const formatName = program.format;
