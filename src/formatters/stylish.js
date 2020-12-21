d824495bcea5f7c7c84027f15844cbfc9f641c3f
almax-21 <almaev21.max@gmail.com>
2020-12-21 22:48:16 +0400

    Change space size && count

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 
-const makeIndent = (size) => ('  ').repeat(size);
+const makeIndent = (size) => (' ').repeat(size);
 
-const spacesCount = 2;
+const spacesCount = 4;
 
 const formatValue = (value, depth) => {
   const iter = (currentValue, currentDepth) => {
